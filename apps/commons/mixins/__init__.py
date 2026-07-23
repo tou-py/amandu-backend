58d85d925e5eede4d2351a3354221ba767436996
tou-py <sebastiantorresg2003@gmail.com>
2026-07-23 17:01:16 -0300

    chore(commons): remove soft delete mixin and tests

--- apps/commons/mixins/__init__.py
+++ apps/commons/mixins/__init__.py
@@ -1,12 +1,8 @@
 from .identity import PublicIdentifierMixin
-from .soft_delete import SoftDeleteManager, SoftDeleteMixin, SoftDeleteQuerySet
 from .timestamp import TimestampMixin
 
 
 __all__ = [
     'PublicIdentifierMixin',
-    'SoftDeleteManager',
-    'SoftDeleteMixin',
-    'SoftDeleteQuerySet',
     'TimestampMixin',
 ]
